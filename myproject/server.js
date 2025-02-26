ecf847af1b988b8ff6a00bdf607b0a1185b9cea6
dimayasinskyi <dmitriymorkov83@gmail.com>
2025-02-26 16:19:59 +0200

    Update server.js

--- myproject/server.js
+++ myproject/server.js
@@ -4,17 +4,17 @@ const fs = require("fs");
 const app = express();
 
 
-app.use(express.static("public"));
+app.use(express.static("myproject/public"));
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.render("index", { title: "Головна", username: "Дмитро" });
-});
+// app.get("/", (req, res) => {
+//     res.render("index", { title: "Головна", username: "Дмитро" });
+// });
 
 const options = {
-    key: fs.readFileSync("key.pem"),
-    cert: fs.readFileSync("cert.pem"),
+    key: fs.readFileSync("myproject/key.pem"),
+    cert: fs.readFileSync("myproject/cert.pem"),
 };
 
 https.createServer(options, app).listen(4000, () => {
